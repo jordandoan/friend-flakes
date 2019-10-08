dd00a9b2e7c2907fedaefee3f80fe21d3343aa78
Jordan <jordandoan@hotmail.com>
2019-10-08 15:39:09 -0700

    Login Page implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import NavBar from './components/Other/NavBar'; 
+import LoginPage from './components/Views/LoginPage';
 
 import './App.scss';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="app-main-container">
       <Route path="/" component={NavBar} />
+      <Route path="/login" component={LoginPage} />
     </div>
   );
 }
