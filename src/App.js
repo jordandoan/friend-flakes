f5156dffb7a3b0684dd0359115ad33b5649fb1ac
Jordan <jordandoan@hotmail.com>
2019-10-11 13:15:05 -0700

    Created view for sign up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/Other/NavBar';
 import LoginPage from './components/Views/LoginPage';
 import Welcome from './components/Views/Welcome';
 import Dashboard from './components/Views/Dashboard';
+import SignupForm from './components/Views/SignupPage';
 
 import './App.scss';
 
@@ -14,6 +15,7 @@ function App() {
     <div className="app-main-container">
       <Route path="/" component={NavBar} />
       <Route path="/login" component={LoginPage} />
+      <Route path="/signup" component={SignupForm} />
       {show && <Route exact path="/" component={Welcome} />}
       {!show && <Route exact path="/" component={Dashboard} />}
     </div>
---
+++ src/components/Views/SignupPage/SignupPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SignupForm from '../../Forms/SignupForm';
+
+import './SignupPage.scss';
+
+const SignupPage = () => {
+    return (
+        <div className="signup-page-container">
+            <SignupForm />
+        </div>
+    )
+}
+
+
+export default SignupPage;
